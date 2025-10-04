485946112ec4644186be2bbf75f3f2489d19f24a
TSPuri <manawat.s@ku.th>
2025-10-04 11:44:48 +0700

    Add files via upload

---
+++ main/java/Main.java
@@ -0,0 +1,8 @@
+
+import Part_UI.GUI.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("djdjd");
+        new Final();
+    }
+}
---
+++ main/java/Part_Service/TaxRuleLoader.java
@@ -0,0 +1,8 @@
+package Part_Service;
+
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import Domain.*;
+// import java.io.File;
+public class TaxRuleLoader {
+    
+}
